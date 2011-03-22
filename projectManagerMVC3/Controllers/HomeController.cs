c8e27b19c7a1566ca60cf9ec25e6b1d9f3ad1f96
Gregor Suttie <gregor.suttie@pulsion.co.uk>
2011-03-22 19:38:55 +0000

    initial project adding

---
+++ projectManagerMVC3/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projectManagerMVC3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Pulsion Project Manager written using ASP.NET MVC 3!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
